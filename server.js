8fbafe3c93940e64b43a2bdf8d5859ce344eed4c
c0d3runn3r <joseph.sullivan@active911.com>
2017-10-24 09:52:35 -0700

    Fix many bugs

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const Jungle=require("./lib/Jungle.js");
 const Player=require("./lib/Player.js");
 const Uuid=require("uuid/v4");
 const Sockhop=require("sockhop");
+// const Sockhop=require("../sockhop/index.js");
 const log = require("bunyan").createLogger({"name":"jungle"});
 const nconf = require('nconf');
+const Victor=require("victor");
 
 
 log.level("debug");
@@ -21,28 +23,46 @@ server
 	.on("connect", (sock)=>{
 
 		// Create new player
-		let p=new Player("Mystery "+jungle.random_name(), Uuid(), sock);
+		let player=new Player(jungle.random_name(), Uuid(), sock);
 
 		// Tell the client who they are
-		server.send(sock, { set: { id : p.id, name: p.name} });
+		server.send(sock, { set: { id : player.id, name: player.name} });
 
 		// Add the player
-		jungle.add_player(p);
-		log.info(`${p.name} connected`);
+		jungle.add_player(player);
+		log.info(`${player.name} connected`);
 
-		// Broadcast the news (after this event exits .. // sockhop bug?)
-		setTimeout(()=>{
+		// Broadcast the news of this new player
 			server.sendall({ 
 				"event" : "add",
 				"type" : "player",
-				"id" : p.id,
+				"id" : player.id,
 				"attributes" : {
 
-					"name" :  p.name,
-					"position" : p.get_position()
+					"name" :  player.name,
+					"position" : player.get_position()
 				}
 			});
-		},0);
+
+		// Send "add" events for all the other players to this player
+		for(let p of jungle._players) {
+
+			// Tell the new player about all the others
+			if(p.id!=player.id){
+
+				server.send(sock, { 
+					"event" : "add",
+					"type" : "player",
+					"id" : p.id,
+					"attributes" : {
+
+						"name" :  p.name,
+						"position" : p.get_position()
+					}
+				 }); 
+			}
+		}
+
 
 	})
 	.on("disconnect", (sock)=>{
@@ -50,7 +70,6 @@ server
 		// Find this player
 		let p=jungle.get_player_by_sock(sock);
 
-		// jungle.delete_player sends data to everyone, which causes a second disconnect event for us.  // sockhop bug?  Anyway, the second time the player has already been removed and p===null.  I suspect Sockhop fires disconnect before removing the socket from its list
 		if(p) {
 
 			// Remove it
@@ -71,12 +90,37 @@ server
 
 		log.error("Sockhop error: "+e);
 	})
-	.on("receive", (obj, meta)=>{
+	.on("receive", (o, meta)=>{
 
-		if(meta.type=="Request"){
 
-			log.error("Received data - don't know what to do with it - "+obj);
-			//meta.callback(self._handle_request(obj, meta.socket));
+		// We got a set request from a player
+		let p=jungle.get_player_by_sock(meta.socket);
+		if (p) {
+
+			if(o.set) {
+
+				// Name change
+				if(o.set.name) {
+
+					p.name=o.set.name;
+					server.broadcast({
+						"event" : "update",
+						"type" : "player",
+						"id" : p.id,
+						"attributes" : {
+							"name" : p.name
+						}
+
+					});
+				}
+
+				// Velocity change
+				if(o.set.velocity && o.set.velocity.x && o.set.velocity.y) {
+
+					p.set_velocity(new Victor(o.set.velocity.x, o.set.velocity.y));
+
+				}
+			}
 		}
 });
 
@@ -84,7 +128,7 @@ server
 jungle.on("move_player", (p)=>{
 
 	server.sendall({ 
-		"event" : "move",
+		"event" : "update",
 		"type" : "player",
 		"id" : p.id,
 		"attributes" : {
@@ -94,15 +138,15 @@ jungle.on("move_player", (p)=>{
 });
 
 
-// Use the runloop to watch the game
-jungle.runloop((jungle /*,dt*/ )=>{
+// // Use the runloop to watch the game
+// jungle.runloop((jungle /*,dt*/ )=>{
 
-	for(let p of jungle.players) {
+// 	for(let p of jungle.players) {
 
-		//console.log(`${p.name} position: ${p.get_position()} velocity: ${p.get_velocity()}`);
-	}
+// 		console.log(`${p.name} position: ${p.get_position()} velocity: ${p.get_velocity()}`);
+// 	}
 
-});
+// });
 
 // Start the game
 server.listen().then(()=>{
